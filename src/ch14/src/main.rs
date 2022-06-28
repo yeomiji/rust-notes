5a4d1fdeeab8ce05aca3c9a5647d1fc856136591
yeomiji <cpdltmqlf@gmail.com>
2022-06-29 01:47:40 +0900

    ch14

---
+++ src/ch14/src/main.rs
@@ -0,0 +1,13 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+fn main() {
+    // optimization levels
+    // #[profile.dev]
+    // #opt-level = 0
+    // #[profile.release]
+    // #opt-level = 3
+    println!("hello world!");
+}
---
+++ src/ch14/workspaces/adder2/src/main.rs
@@ -0,0 +1,11 @@
+use add_one2::add_one;
+use rand;
+
+fn main() {
+    let num = 10;
+    println!(
+        "Hello, world! {} plus one is {}!",
+        num,
+        add_one(num)
+    );
+}
